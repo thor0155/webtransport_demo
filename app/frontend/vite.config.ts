62817f3eb68dbd085ebf2e20d071edea5a5ae1f4
ThorLin <thor0155@gmail.com>
2026-09-09 15:52:19 +0800

    fix: HTTPS can be omitted when developing the front end.

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from "fs";
+// import fs from "fs";
 import path from "path";
 
 // https://vite.dev/config/
@@ -16,17 +16,15 @@ export default defineConfig(({ command }) => ({
       },
   },
   server:  command === 'serve' ?{
-        // host:"0.0.0.0",
         host:"localhost",
         port:5173,
-        https:{
-            cert: fs.readFileSync(
-                "../../localhost+2.pem"
-            ),
-
-            key: fs.readFileSync(
-                "../../localhost+2-key.pem"
-            )
-        }
+        // https:{
+        //     cert: fs.readFileSync(
+        //         "../../localhost-cert.pem"
+        //     ),
+        //     key: fs.readFileSync(
+        //         "../../localhost-key.pem"
+        //     )
+        // }
     }: undefined,
 }))
